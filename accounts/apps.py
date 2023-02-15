231cbc756dbbab29deddbaa0d6a0b606328b7ee4
wshih <wen.shih@alumni.stonybrook.edu>
2023-02-15 02:06:50 -0800

    Install django rest framework

---
+++ accounts/apps.py
@@ -0,0 +1,17 @@
+# =================================================================================================
+#                                  All Rights Reserved.
+# =================================================================================================
+# File description:
+#
+# =================================================================================================
+#    Date      Name                    Description of Change
+# 15-Feb-2023  Wayne Shih              Initial create
+# $HISTORY$
+# =================================================================================================
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
